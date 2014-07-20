a7886e16bf614b6b9a9115c7cc5ec9915a014ad6
chaishushan@gmail.com <chaishushan@gmail.com>
2014-07-20 11:59:50 +0800

    image/convert: implements.

--- image/convert/convert.go
+++ image/convert/convert.go
@@ -9,46 +9,425 @@ import (
 	"image"
 	"image/color"
 
+	image_ext "github.com/chai2010/gopkg/image"
 	color_ext "github.com/chai2010/gopkg/image/color"
 )
 
 func Gray(m image.Image) *image.Gray {
-	return nil
+	if gray, ok := m.(*image.Gray); ok {
+		return gray
+	}
+	b := m.Bounds()
+	gray := image.NewGray(b)
+	switch m := m.(type) {
+	case *image.Gray16:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.Gray16At(x, y)
+				gray.SetGray(x, y, color.Gray{uint8(v.Y >> 8)})
+			}
+		}
+	case *image.RGBA:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				gray.SetGray(x, y, color.GrayModel.Convert(m.RGBAAt(x, y)).(color.Gray))
+			}
+		}
+	case *image.RGBA64:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				gray.SetGray(x, y, color.GrayModel.Convert(m.RGBA64At(x, y)).(color.Gray))
+			}
+		}
+	case *image.YCbCr:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			copy(
+				gray.Pix[y*gray.Stride:][:gray.Stride],
+				m.Y[y*m.YStride:][:m.YStride],
+			)
+		}
+	default:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				gray.Set(x, y, m.At(x, y))
+			}
+		}
+	}
+	return gray
 }
 
 func Gray16(m image.Image) *image.Gray16 {
-	return nil
+	if gray16, ok := m.(*image.Gray16); ok {
+		return gray16
+	}
+	b := m.Bounds()
+	gray16 := image.NewGray16(b)
+	switch m := m.(type) {
+	case *image.Gray:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.GrayAt(x, y)
+				gray16.SetGray16(x, y, color.Gray16{uint16(v.Y) << 8})
+			}
+		}
+	case *image.RGBA:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				gray16.SetGray16(x, y,
+					color.Gray16Model.Convert(m.RGBAAt(x, y)).(color.Gray16),
+				)
+			}
+		}
+	case *image.RGBA64:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				gray16.SetGray16(x, y,
+					color.Gray16Model.Convert(m.RGBA64At(x, y)).(color.Gray16),
+				)
+			}
+		}
+	case *image.YCbCr:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.Y[m.YOffset(x, y)]
+				gray16.SetGray16(x, y, color.Gray16{uint16(v) << 8})
+			}
+		}
+	default:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				gray16.Set(x, y, m.At(x, y))
+			}
+		}
+	}
+	return gray16
 }
 
 func Gray32f(m image.Image) *image_ext.Gray32f {
-	return nil
+	if gray32f, ok := m.(*image_ext.Gray32f); ok {
+		return gray32f
+	}
+	b := m.Bounds()
+	gray32f := image_ext.NewGray32f(b)
+	for y := b.Min.Y; y < b.Max.Y; y++ {
+		for x := b.Min.X; x < b.Max.X; x++ {
+			gray32f.Set(x, y, m.At(x, y))
+		}
+	}
+	return gray32f
 }
 
 func RGB(m image.Image) *image_ext.RGB {
-	return nil
+	if rgb, ok := m.(*image_ext.RGB); ok {
+		return rgb
+	}
+	b := m.Bounds()
+	rgb := image_ext.NewRGB(b)
+	switch m := m.(type) {
+	case *image.Gray:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.GrayAt(x, y)
+				rgb.SetRGB(x, y, color_ext.RGB{
+					R: v.Y,
+					G: v.Y,
+					B: v.Y,
+				})
+			}
+		}
+	case *image.Gray16:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.Gray16At(x, y)
+				rgb.SetRGB(x, y, color_ext.RGB{
+					R: uint8(v.Y >> 8),
+					G: uint8(v.Y >> 8),
+					B: uint8(v.Y >> 8),
+				})
+			}
+		}
+	default:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				rgb.Set(x, y, m.At(x, y))
+			}
+		}
+	}
+	return rgb
 }
 
 func RGB48(m image.Image) *image_ext.RGB48 {
-	return nil
+	if rgb48, ok := m.(*image_ext.RGB48); ok {
+		return rgb48
+	}
+	b := m.Bounds()
+	rgb48 := image_ext.NewRGB48(b)
+	switch m := m.(type) {
+	case *image.Gray:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.GrayAt(x, y)
+				rgb48.SetRGB48(x, y, color_ext.RGB48{
+					R: uint16(v.Y) >> 8,
+					G: uint16(v.Y) >> 8,
+					B: uint16(v.Y) >> 8,
+				})
+			}
+		}
+	case *image.Gray16:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.Gray16At(x, y)
+				rgb48.SetRGB48(x, y, color_ext.RGB48{
+					R: v.Y,
+					G: v.Y,
+					B: v.Y,
+				})
+			}
+		}
+	default:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				rgb48.Set(x, y, m.At(x, y))
+			}
+		}
+	}
+	return rgb48
 }
 
 func RGB96f(m image.Image) *image_ext.RGB96f {
-	return nil
+	if rgb96f, ok := m.(*image_ext.RGB96f); ok {
+		return rgb96f
+	}
+	b := m.Bounds()
+	rgb96f := image_ext.NewRGB96f(b)
+	switch m := m.(type) {
+	case *image.Gray:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.GrayAt(x, y)
+				rgb96f.SetRGB96f(x, y, color_ext.RGB96f{
+					R: float32(uint16(v.Y) >> 8),
+					G: float32(uint16(v.Y) >> 8),
+					B: float32(uint16(v.Y) >> 8),
+				})
+			}
+		}
+	case *image.Gray16:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.Gray16At(x, y)
+				rgb96f.SetRGB96f(x, y, color_ext.RGB96f{
+					R: float32(v.Y),
+					G: float32(v.Y),
+					B: float32(v.Y),
+				})
+			}
+		}
+	default:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				rgb96f.Set(x, y, m.At(x, y))
+			}
+		}
+	}
+	return rgb96f
 }
 
 func RGBA(m image.Image) *image.RGBA {
-	return nil
+	if rgba, ok := m.(*image.RGBA); ok {
+		return rgba
+	}
+	b := m.Bounds()
+	rgba := image.NewRGBA(b)
+	switch m := m.(type) {
+	case *image.Gray:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.GrayAt(x, y)
+				rgba.SetRGBA(x, y, color.RGBA{
+					R: v.Y,
+					G: v.Y,
+					B: v.Y,
+					A: 0xFF,
+				})
+			}
+		}
+	case *image.Gray16:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.Gray16At(x, y)
+				rgba.SetRGBA(x, y, color.RGBA{
+					R: uint8(v.Y >> 8),
+					G: uint8(v.Y >> 8),
+					B: uint8(v.Y >> 8),
+					A: 0xFF,
+				})
+			}
+		}
+	default:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				rgba.Set(x, y, m.At(x, y))
+			}
+		}
+	}
+	return rgba
 }
 
 func RGBA64(m image.Image) *image.RGBA64 {
-	return nil
+	if rgba64, ok := m.(*image.RGBA64); ok {
+		return rgba64
+	}
+	b := m.Bounds()
+	rgba64 := image.NewRGBA64(b)
+	switch m := m.(type) {
+	case *image.Gray:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.GrayAt(x, y)
+				rgba64.SetRGBA64(x, y, color.RGBA64{
+					R: uint16(v.Y) >> 8,
+					G: uint16(v.Y) >> 8,
+					B: uint16(v.Y) >> 8,
+					A: 0xFFFF,
+				})
+			}
+		}
+	case *image.Gray16:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.Gray16At(x, y)
+				rgba64.SetRGBA64(x, y, color.RGBA64{
+					R: v.Y,
+					G: v.Y,
+					B: v.Y,
+					A: 0xFFFF,
+				})
+			}
+		}
+	case *image.RGBA:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.RGBAAt(x, y)
+				rgba64.SetRGBA64(x, y, color.RGBA64{
+					R: uint16(v.R) >> 8,
+					G: uint16(v.G) >> 8,
+					B: uint16(v.B) >> 8,
+					A: uint16(v.A) >> 8,
+				})
+			}
+		}
+	default:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				rgba64.Set(x, y, m.At(x, y))
+			}
+		}
+	}
+	return rgba64
 }
 
 func RGBA128f(m image.Image) *image_ext.RGBA128f {
-	return nil
+	if rgba128f, ok := m.(*image_ext.RGBA128f); ok {
+		return rgba128f
+	}
+	b := m.Bounds()
+	rgba128f := image_ext.NewRGBA128f(b)
+	switch m := m.(type) {
+	case *image.Gray:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.GrayAt(x, y)
+				rgba128f.SetRGBA128f(x, y, color_ext.RGBA128f{
+					R: float32(uint16(v.Y) >> 8),
+					G: float32(uint16(v.Y) >> 8),
+					B: float32(uint16(v.Y) >> 8),
+					A: 0xFFFF,
+				})
+			}
+		}
+	case *image.Gray16:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.Gray16At(x, y)
+				rgba128f.SetRGBA128f(x, y, color_ext.RGBA128f{
+					R: float32(v.Y),
+					G: float32(v.Y),
+					B: float32(v.Y),
+					A: 0xFFFF,
+				})
+			}
+		}
+	case *image.RGBA:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				v := m.RGBAAt(x, y)
+				rgba128f.SetRGBA128f(x, y, color_ext.RGBA128f{
+					R: float32(uint16(v.R) >> 8),
+					G: float32(uint16(v.G) >> 8),
+					B: float32(uint16(v.B) >> 8),
+					A: float32(uint16(v.A) >> 8),
+				})
+			}
+		}
+	default:
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
+				rgba128f.Set(x, y, m.At(x, y))
+			}
+		}
+	}
+	return rgba128f
 }
 
-func Convert(m image.Image, isColor bool) image.Image {
+func Paletted(m image.Image, p color.Palette) *image.Paletted {
+	if m, ok := m.(*image.Paletted); ok {
+		if len(m.Palette) == len(p) {
+			if &m.Palette[0] == &p[0] {
+				return m
+			}
+			isEqual := true
+			for i, c := range m.Palette {
+				r0, g0, b0, a0 := c.RGBA()
+				r1, g1, b1, a1 := p[i].RGBA()
+				if r0 != r1 || g0 != g1 || b0 != b1 || a0 != a1 {
+					isEqual = false
+					break
+				}
+			}
+			if isEqual {
+				return m
+			}
+		}
+	}
+	b := m.Bounds()
+	paletted := image.NewPaletted(b, p)
+	for y := b.Min.Y; y < b.Max.Y; y++ {
+		for x := b.Min.X; x < b.Max.X; x++ {
+			paletted.Set(x, y, m.At(x, y))
+		}
+	}
+	return paletted
+}
+
+func YCbCr(m image.Image, subsampleRatio image.YCbCrSubsampleRatio) *image.YCbCr {
+	if m, ok := m.(*image.YCbCr); ok {
+		if m.SubsampleRatio == subsampleRatio {
+			return m
+		}
+	}
+	b := m.Bounds()
+	yCbCr := image_ext.NewYCbCr(b, subsampleRatio)
+	for y := b.Min.Y; y < b.Max.Y; y++ {
+		for x := b.Min.X; x < b.Max.X; x++ {
+			yCbCr.Set(x, y, m.At(x, y))
+		}
+	}
+	return (*image.YCbCr)(yCbCr)
+}
+
+func Color(m image.Image, isColor bool) image.Image {
 	if isColor {
 		return convertToColor(m)
 	} else {
@@ -86,7 +465,7 @@ func convertToColor(m image.Image) image.Image {
 			}
 		}
 		return rgb48
-	case *Gray32f:
+	case *image_ext.Gray32f:
 		b := m.Bounds()
 		rgb96f := image_ext.NewRGB96f(b)
 		for y := b.Min.Y; y < b.Max.Y; y++ {
@@ -181,7 +560,7 @@ func convertToColor(m image.Image) image.Image {
 
 func convertToGray(m image.Image) image.Image {
 	switch m := m.(type) {
-	case *RGB:
+	case *image_ext.RGB:
 		b := m.Bounds()
 		gray := image.NewGray(b)
 		for y := b.Min.Y; y < b.Max.Y; y++ {
@@ -192,7 +571,7 @@ func convertToGray(m image.Image) image.Image {
 			}
 		}
 		return gray
-	case *RGB48:
+	case *image_ext.RGB48:
 		b := m.Bounds()
 		gray16 := image.NewGray16(b)
 		for y := b.Min.Y; y < b.Max.Y; y++ {
@@ -203,7 +582,7 @@ func convertToGray(m image.Image) image.Image {
 			}
 		}
 		return gray16
-	case *RGB96f:
+	case *image_ext.RGB96f:
 		b := m.Bounds()
 		gray32f := image_ext.NewGray32f(b)
 		for y := b.Min.Y; y < b.Max.Y; y++ {
@@ -236,7 +615,7 @@ func convertToGray(m image.Image) image.Image {
 			}
 		}
 		return gray16
-	case *RGBA128f:
+	case *image_ext.RGBA128f:
 		b := m.Bounds()
 		gray32f := image_ext.NewGray32f(b)
 		for y := b.Min.Y; y < b.Max.Y; y++ {
